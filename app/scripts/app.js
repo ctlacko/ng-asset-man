a62b362aa10ba8dea5f4c1a88d266c0a34753a6e
Chris Lacko <ctlacko@gmail.com>
2014-02-04 21:43:21 -0600

    Instantiated Firebase and added AngularFire.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,13 +4,24 @@ angular.module('rtAssetManagerApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
+  'firebase',
   'ngRoute'
 ])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $locationProvider) {
+    // this locationProvider configuration removes the default
+    // hash prefix for URLs, but angular will still fallback to using
+    // the hash prefix on legacy browsers. i.e. /#/sandbox
+    // Reference: http://docs.angularjs.org/guide/dev_guide.services.$location
+    $locationProvider
+      .html5Mode(true);
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
+      })
+      .when('/sandbox', {
+        templateUrl: 'views/sandbox.html',
+        controller: 'SandboxCtrl'
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,16 @@
+/*tell jsHint that Firebase is defined globally*/
+/*global Firebase:false */
 'use strict';
 
 angular.module('rtAssetManagerApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $firebase) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    console.log($firebase);
+    var ref = new Firebase('https://m84x3ofsh2b.firebaseio-demo.com/');
+    $scope.messages = $firebase(ref);
+    console.log($scope.messages);
   });
